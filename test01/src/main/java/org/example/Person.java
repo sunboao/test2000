2712322719c02714e4b9762f7dbcc8c6e256a84e
sunboao <2716922184@qq.com>
2021-12-04 10:35:27 +0800

    123

---
+++ test01/src/main/java/org/example/Person.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Person {
+    private Integer age;
+    private String name;
+    private String address;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
